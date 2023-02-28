b4c9150c9f61363974240cc4486ed9a3ca39badc
SKornya <kornyakovS@gmail.com>
2023-02-28 22:19:17 +0300

    realize error validation

--- src/script/view.js
+++ src/script/view.js
@@ -1,3 +1,13 @@
-export default (state) => {
-  // console.log(state);
+export const render = () => {
+  console.log('plug');
+};
+
+export const errorRender = (classes, value) => {
+  const container = document.querySelector('.col-md-10');
+  const input = document.querySelector('input');
+  input.classList.add('is-invalid');
+  const p = document.createElement('p');
+  p.classList.add(...classes);
+  p.textContent = value;
+  container.append(p);
 };
