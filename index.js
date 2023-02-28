1e10141bd0bbc57f88dd86294062689f0655e3d9
SKornya <kornyakovS@gmail.com>
2023-02-28 21:16:03 +0300

    change path to import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import app from './src/view.js';
+import app from './src/script/model.js';
 import './src/style/index.css';
 
 app();
