aa05bcaf0adf3d903a083288d7ef1ad2b5326775
SKornya <kornyakovS@gmail.com>
2023-02-28 23:20:24 +0300

    try to use yup locale

--- src/script/model.js
+++ src/script/model.js
@@ -1,12 +1,20 @@
 import * as yup from 'yup';
+import * as i18next from 'i18next';
 // import axios from 'axios';
 import watchedState from './controller.js';
+
+yup.setLocale({
+  string: {
+    url: 'Ссылка должна быть валидным URL',
+    notOneOf: 'RSS уже существует',
+  },
+});
 
 const validateSchema = yup.string()
   .trim()
   .url()
-  .required()
-  .notOneOf(watchedState.feeds);
+  .notOneOf(watchedState.feeds)
+  .required();
 
 const validate = (url) => {
   validateSchema.validate(url)
@@ -14,7 +22,7 @@ const validate = (url) => {
       watchedState.feeds.push(url);
     })
     .catch((err) => {
-      watchedState.error = err;
+      watchedState.error = err.message;
     });
 };
 
