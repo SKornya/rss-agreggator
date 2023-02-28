f1b4ff80adaaa26fcb1c269fc69597a4cc778b55
SKornya <kornyakovS@gmail.com>
2023-02-28 23:21:00 +0300

    add refresh container

--- src/script/view.js
+++ src/script/view.js
@@ -3,7 +3,11 @@ export const render = () => {
 };
 
 export const errorRender = (classes, value) => {
+  const errorsP = document.querySelectorAll('.text-danger');
   const container = document.querySelector('.col-md-10');
+  errorsP.forEach((p) => {
+    p.remove();
+  });
   const input = document.querySelector('input');
   input.classList.add('is-invalid');
   const p = document.createElement('p');
