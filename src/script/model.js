002480eae7a276b28a6842f5b2fe186cd1def355
SKornya <kornyakovS@gmail.com>
2023-03-01 10:01:54 +0300

    add i18next

--- src/script/model.js
+++ src/script/model.js
@@ -2,10 +2,13 @@ import * as yup from 'yup';
 import * as i18next from 'i18next';
 // import axios from 'axios';
 import watchedState from './controller.js';
+import resources from '.src'
 
 yup.setLocale({
   string: {
     url: 'Ссылка должна быть валидным URL',
+  },
+  mixed: {
     notOneOf: 'RSS уже существует',
   },
 });
@@ -26,7 +29,7 @@ const validate = (url) => {
     });
 };
 
-export default () => {
+const app = () => {
   const form = document.querySelector('form');
 
   form.addEventListener('submit', (e) => {
@@ -35,4 +38,13 @@ export default () => {
     validate(t);
     form.reset();
   });
+};
+
+export default () => {
+  const i18nInstance = i18next.createInstance();
+  i18nInstance.init({
+    lng: 'ru',
+    resources,
+  });
+  app(i18nInstance);
 };
