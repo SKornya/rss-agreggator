fbf1ce83094b7fad407d3fac2ccc98cadcb5dec9
SKornya <kornyakovS@gmail.com>
2023-03-03 15:54:07 +0300

    remove controller.js, replce render funcs, state

--- src/script/model.js
+++ src/script/model.js
@@ -1,38 +1,48 @@
 import * as yup from 'yup';
 import * as i18next from 'i18next';
 import axios from 'axios';
-import watchedState from './controller.js';
+import getWatchedState from './view.js';
+// import watchedState from './view.js';
 import resources from './locale/resources.js';
 import parseData from './parser.js';
-
-yup.setLocale({
-  string: {
-    url: ({ url }) => ({ key: 'notValidURL', values: { url } }),
-  },
-  mixed: {
-    notOneOf: ({ notOneOf }) => ({ key: 'oneOfFeeds', values: { notOneOf } }),
-  },
-});
-
-const validateSchema = yup.string()
-  .trim()
-  .url()
-  .notOneOf(watchedState.feeds)
-  .required();
+import { get } from 'lodash';
 
 const app = (i18n) => {
+  const state = {
+    urls: ['https://ru.hexlet.io/lessons.rss'],
+    error: '',
+  };
+
+  const watchedState = getWatchedState(state);
+
+  yup.setLocale({
+    string: {
+      url: ({ url }) => ({ key: 'notValidURL', values: { url } }),
+    },
+    mixed: {
+      notOneOf: ({ notOneOf }) => ({ key: 'oneOfFeeds', values: { notOneOf } }),
+    },
+  });
+  
+  const validateSchema = yup.string()
+    .trim()
+    .url()
+    .notOneOf(state.urls)
+    .required();
+
   const validate = (url) => {
-    validateSchema.validate(url)
+    validateSchema.validate(url)  
       .then((res) => {
-        watchedState.feeds.push(url);
-        axios.get(url)
-          .then(({ data }) => {
-            const htmlData = parseData(data);
-            console.log(htmlData);
-          })
-          .catch((e) => console.log(e));
-      })  
+        watchedState.urls.push(url);
+        // axios.get(url)
+        //   .then(({ data }) => {
+        //     const htmlData = parseData(data);
+        //     console.log(htmlData);
+        //   })
+        //   .catch((e) => console.log(e));
+      })
       .catch((err) => {
+        console.log(err);
         const [error] = err.errors;
         const { key } = error;
         watchedState.error = i18n.t(key);
@@ -43,10 +53,10 @@ const app = (i18n) => {
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+    form.focus();
     const t = new FormData(form).get('url');
     validate(t);
     form.reset();
-    form.focus();
   });
 };
 
--- src/script/view.js
+++ src/script/view.js
@@ -1,17 +1,59 @@
-export const render = () => {
+import onChange from 'on-change';
+import axios from 'axios';
+// import { errorRender } from './view.js';
+import { conformsTo, isElement, valuesIn } from 'lodash';
+import { resolveConfig } from 'prettier';
+
+const render = () => {
   console.log('plug');
 };
 
-export const errorRender = (classes, value) => {
-  const errorsP = document.querySelectorAll('.text-danger');
-  const container = document.querySelector('.col-md-10');
-  errorsP.forEach((p) => {
-    p.remove();
+const errorRender = (value) => {
+  const exampleP = document.querySelector('.text-muted');
+  const feedbackP = exampleP.nextElementSibling;
+  if (feedbackP) {
+    const isError = feedbackP.classList.contains('text-danger');
+    if (!isError) {
+      feedbackP.classList.add('text-danger');
+    }
+    feedbackP.textContent = value;
+    return;
+  }
+  const newFeedbackP = document.createElement('p');
+  newFeedbackP.classList.add('feedback', 'm-0', 'position-absolute', 'small', 'text-success', 'text-danger');
+  newFeedbackP.textContent = value;
+  exampleP.after(newFeedbackP);
+};
+
+const renderSucsess = () => {
+  const exampleP = document.querySelector('.text-muted');
+  const feedbackP = exampleP.nextElementSibling;
+  if (feedbackP) {
+    const isError = feedbackP.classList.contains('text-danger');
+    if (isError) {
+      feedbackP.classList.replace('text-danger', 'text-success');
+    }
+    feedbackP.textContent = 'RSS успешно загружен';
+    return;
+  }
+  const newFeedbackP = document.createElement('p');
+  newFeedbackP.classList.add('feedback', 'm-0', 'position-absolute', 'small', 'text-success');
+  newFeedbackP.textContent = 'RSS успешно загружен';
+  exampleP.after(newFeedbackP);
+};
+
+export default (state) => {
+  const watchedState = onChange(state, (path, value) => {
+    // const classes = ['feedback', 'm-0', 'position-absolute', 'small', 'text-success'];
+    if (path === 'error') {
+      // classes.push('text-danger');
+      errorRender(value);
+    }
+    if (path === 'urls') {
+      console.log(value);
+      renderSucsess();
+    }
   });
-  const input = document.querySelector('input');
-  input.classList.add('is-invalid');
-  const p = document.createElement('p');
-  p.classList.add(...classes);
-  p.textContent = value;
-  container.append(p);
+
+  return watchedState;
 };
