79fbdcc10e9624c54bb3adc58f6848c7fa6a523b
SKornya <kornyakovS@gmail.com>
2023-03-05 23:11:43 +0300

    add translations

--- src/script/locale/ru.js
+++ src/script/locale/ru.js
@@ -1,5 +1,11 @@
 export default {
   translation: {
+    title: 'RSS агрегатор',
+    lead: 'Начните читать RSS сегодня! Это легко и красиво',
+    placeholder: 'Ссылка RSS',
+    addButton: 'Добавить',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    successLoad: 'RSS успешно загружен',
     notValidURL: 'Ссылка должна быть валидным URL',
     oneOfFeeds: 'RSS уже существует',
   },
