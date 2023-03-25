52a0eb3395befbc0100a2f475c9ac8ae218fec4a
SKornya <kornyakovS@gmail.com>
2023-03-26 01:22:34 +0300

    refactoring

--- src/script/view.js
+++ src/script/view.js
@@ -8,7 +8,7 @@ i18n.init({
   resources,
 });
 
-export const init = () => {
+export const mainPageRender = () => {
   const fields = {
     title: document.querySelector('h1'),
     lead: document.querySelector('.lead'),
@@ -25,108 +25,29 @@ export const init = () => {
   input.setAttribute('placeholder', i18n.t('init.placeholder'));
 };
 
-// const feedbackRender = (value, type) => {
-//   const exampleP = document.querySelector('.text-muted');
-//   const feedbackP = exampleP.nextElementSibling;
-//   if (feedbackP) {
-//     const isError = feedbackP.classList.contains('text-danger');
-
-//     if (isError) {
-//       feedbackP.classList.replace('text-danger', 'text-success');
-//       feedbackP.textContent = i18n.t('successLoad');
-//     } else {
-//       feedbackP.classList.add('text-danger');
-//       feedbackP.textContent = i18n.t(`errors.${value}`);
-//     }
-//     return;
-//   }
-//   const newFeedbackP = document.createElement('p');
-//   newFeedbackP.classList.add('feedback', 'm-0', 'position-absolute', 'small', 'text-success', 'text-danger');
-
-//   newFeedbackP.textContent = i18n.t(`errors.${value}`);
-//   exampleP.after(newFeedbackP);
-// };
-
-const createNewFeedbackElement = (prevSimbling, content, type) => {
-  const newFeedbackP = document.createElement('p');
-  newFeedbackP.classList.add('feedback', 'm-0', 'position-absolute', 'small', 'text-success');
-  if (type === 'error') {
-    newFeedbackP.classList.add('text-danger');
+const feedbackRender = (value, type) => {
+  const feedback = document.querySelector('.feedback');
+  switch (type) {
+    case 'error':
+      if (feedback.classList.contains('text-success')) {
+        feedback.classList.replace('text-success', 'text-danger');
+      }
+      break;
+    case 'success':
+      if (feedback.classList.contains('text-danger')) {
+        feedback.classList.replace('text-danger', 'text-success');
+      }
+      break;
+    default:
+      throw new Error('unknow state');
   }
-  newFeedbackP.textContent = i18n.t(`errors.${content}`);
-  prevSimbling.after(newFeedbackP);
-};
-
-// const rerenderFeedbackElement = (element) => {
-//   const isError = feedbackP.classList.contains('text-danger');
-//   if (!isError) {
-//     feedbackP.classList.add('text-danger');
-//     feedbackP.textContent = i18n.t(`${value}`);
-//   } else {
-//     feedbackP.classList.toggle('text-danger');
-//     feedbackP.textContent = i18n.t('successLoad');
-//   }
-//   return;
-// };
-
-const errorRender = (value) => {
-  const exampleP = document.querySelector('.text-muted');
-  const feedbackP = exampleP.nextElementSibling;
-  if (feedbackP) {
-    const isError = feedbackP.classList.contains('text-danger');
-    if (!isError) {
-      feedbackP.classList.add('text-danger');
-    }
-    feedbackP.textContent = i18n.t(`errors.${value}`);
-    return;
-  }
-  createNewFeedbackElement(exampleP, value, 'error');
-  // const newFeedbackP = document.createElement('p');
-  // newFeedbackP.classList.add('feedback', 'm-0', 'position-absolute', 'small', 'text-success', 'text-danger');
-  // newFeedbackP.textContent = i18n.t(`${value}`);
-  // exampleP.after(newFeedbackP);
-};
-
-const renderSuccess = (value) => {
-  const exampleP = document.querySelector('.text-muted');
-  const feedbackP = exampleP.nextElementSibling;
-  if (feedbackP) {
-    const isError = feedbackP.classList.contains('text-danger');
-    if (isError) {
-      feedbackP.classList.replace('text-danger', 'text-success');
-    }
-    feedbackP.textContent = i18n.t(`${value}`);
-    return;
-  }
-  createNewFeedbackElement(exampleP, value, 'success');
-  // const newFeedbackP = document.createElement('p');
-  // newFeedbackP.classList.add('feedback', 'm-0', 'position-absolute', 'small', 'text-success');
-  // newFeedbackP.textContent = i18n.t('successLoad');
-  // exampleP.after(newFeedbackP);
+  feedback.textContent = i18n.t(`${type}.${value}`);
 };
 
 export default (state) => {
   const watchedState = onChange(state, (path, value) => {
     if (path === 'form.state') {
-      switch (value) {
-        case 'success':
-          renderSuccess(state.form.message);
-          break;
-        case 'error':
-          errorRender(state.form.message);
-          break;
-        default:
-          throw new Error('unknow state');
-      }
-    }
-
-    if (path === 'feedback') {
-      // feedbackRender(value, 'error');
-      errorRender(value);
-    }
-    if (path === 'urls') {
-      // feedbackRender(null, 'success');
-      renderSucsess(state.feedback);
+      feedbackRender(state.form.message, value);
     }
   });
 
