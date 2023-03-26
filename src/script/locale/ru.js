190d05b027c705d69fea97d6f794c917be3c465d
SKornya <kornyakovS@gmail.com>
2023-03-26 21:49:42 +0300

    add new keys

--- src/script/locale/ru.js
+++ src/script/locale/ru.js
@@ -2,7 +2,7 @@ export default {
   translation: {
     init: {
       title: 'RSS агрегатор',
-      lead: 'Начните читать RSS сегодня! Это легко и красиво.',
+      lead: 'Начните читать RSS сегодня! Это легко, это красиво.',
       inputLabel: 'Ссылка RSS',
       addButton: 'Добавить',
       example: 'Пример: https://ru.hexlet.io/lessons.rss',
@@ -16,5 +16,8 @@ export default {
     success: {
       successLoad: 'RSS успешно загружен',
     },
+    posts: 'Посты',
+    feeds: 'Фиды',
+    watchButton: 'Просмотр',
   },
 };
