eb6219f72ab276362871a2b7447bf99deb5686e7
SKornya <kornyakovS@gmail.com>
2023-04-07 20:52:11 +0300

    replace files to other directories

---
+++ src/locales/ru.js
@@ -0,0 +1,23 @@
+export default {
+  translation: {
+    init: {
+      title: 'RSS агрегатор',
+      lead: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      inputLabel: 'Ссылка RSS',
+      addButton: 'Добавить',
+      example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    },
+    error: {
+      notValidURL: 'Ссылка должна быть валидным URL',
+      oneOfFeeds: 'RSS уже существует',
+      networkError: 'Ошибка сети',
+      parsererror: 'Ресурс не содержит валидный RSS',
+    },
+    loaded: {
+      success: 'RSS успешно загружен',
+    },
+    posts: 'Посты',
+    feeds: 'Фиды',
+    watchButton: 'Просмотр',
+  },
+};
