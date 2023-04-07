ce4f451d3e1e55806d9c52aaf4926386269561c5
SKornya <kornyakovS@gmail.com>
2023-04-07 21:49:10 +0300

    refactor

--- src/app.js
+++ src/app.js
@@ -7,6 +7,50 @@ import parseData from './parser.js';
 
 const getOriginURL = (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
 
+const updateRSS = (url, state, watchedState) => {
+  axios.get(getOriginURL(url.trim()))
+    .then((response) => {
+      const [feed, posts] = parseData(response.data.contents);
+      const feedId = state.feeds.find((item) => item.link === feed.link).id;
+      const postsFromState = state.posts.filter((post) => post.feedId === feedId);
+      const newPosts = _.differenceBy(posts, postsFromState, 'link');
+      newPosts.forEach((post) => {
+        post.id = _.uniqueId();
+        post.feedId = feedId;
+      });
+      watchedState.posts = [...newPosts, ...state.posts];
+    })
+    .catch(() => []);
+  setTimeout(updateRSS, 5000, url);
+};
+
+const getRequest = (url, state, watchedState) => {
+  axios.get(getOriginURL(url.trim()))
+    .then((response) => {
+      const [feed, posts] = parseData(response.data.contents);
+      watchedState.proceedState = 'loaded';
+      watchedState.urls.push(url);
+      feed.id = _.uniqueId();
+      feed.link = url;
+      const feedId = feed.id;
+      watchedState.feeds.push(feed);
+      posts.forEach((post) => {
+        post.id = _.uniqueId();
+        post.feedId = feedId;
+      });
+      watchedState.posts = [...posts, ...state.posts];
+      updateRSS(url, state, watchedState);
+    })
+    .catch((err) => {
+      if (!(_.has(err, 'errors'))) {
+        watchedState.form.error = 'networkError';
+      } else {
+        watchedState.form.error = 'parsererror';
+      }
+      watchedState.proceedState = 'failed';
+    });
+};
+
 export default () => {
   const state = {
     proceedState: '',
@@ -20,50 +64,6 @@ export default () => {
 
   const watchedState = getWatchedState(state);
   watchedState.proceedState = 'filling';
-
-  const updateRSS = (url) => {
-    axios.get(getOriginURL(url.trim()))
-      .then((response) => {
-        const [feed, posts] = parseData(response.data.contents);
-        const feedId = state.feeds.find((item) => item.link === feed.link).id;
-        const postsFromState = state.posts.filter((post) => post.feedId === feedId);
-        const newPosts = _.differenceBy(posts, postsFromState, 'link');
-        newPosts.forEach((post) => {
-          post.id = _.uniqueId();
-          post.feedId = feedId;
-        });
-        watchedState.posts = [...newPosts, ...state.posts];
-      })
-      .catch(() => []);
-    setTimeout(updateRSS, 5000, url);
-  };
-
-  const getRequest = (url) => {
-    axios.get(getOriginURL(url.trim()))
-      .then((response) => {
-        const [feed, posts] = parseData(response.data.contents);
-        watchedState.proceedState = 'loaded';
-        watchedState.urls.push(url);
-        feed.id = _.uniqueId();
-        feed.link = url;
-        const feedId = feed.id;
-        watchedState.feeds.push(feed);
-        posts.forEach((post) => {
-          post.id = _.uniqueId();
-          post.feedId = feedId;
-        });
-        watchedState.posts = [...posts, ...state.posts];
-        updateRSS(url);
-      })
-      .catch((err) => {
-        if (!(_.has(err, 'errors'))) {
-          watchedState.form.error = 'networkError';
-        } else {
-          watchedState.form.error = 'parsererror';
-        }
-        watchedState.proceedState = 'failed';
-      });
-  };
 
   yup.setLocale({
     string: {
@@ -84,7 +84,7 @@ export default () => {
     validateSchema.validate(url)
       .then(() => {
         watchedState.proceedState = 'loading';
-        getRequest(url);
+        getRequest(url, state, watchedState);
       })
       .catch((err) => {
         const [error] = err.errors;
